ee2e0331cf06a19937ae8e9ebbae701528e667db
N S <kwb020312@naver.com>
2023-05-08 05:36:20 +0900

    Feat-Client_Side_Setup

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-5xl p-3">Avax Gods</h1>
+      <h2 className="text-3xl p-3">Web3.0 NFT 전략 카드게임</h2>
+    </div>
+  );
+};
+
+export default Home;
