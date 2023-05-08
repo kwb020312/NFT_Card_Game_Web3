cee957110f9dd7941d251acf572e8791ae39a14e
N S <kwb020312@naver.com>
2023-05-08 21:27:00 +0900

    Feat-환영문구

--- client/src/page/CreateBattle.jsx
+++ client/src/page/CreateBattle.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import { PageHOC } from "../components";
 
 const CreateBattle = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-white text-xl">방을 생성하고 대화해보세요.</h1>
+    </div>
+  );
 };
 
 export default PageHOC(
