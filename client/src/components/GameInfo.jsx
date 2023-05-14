3bfea58735ca9f877609a35fab2f69243be2e8e7
N S <kwb020312@naver.com>
2023-05-14 14:23:44 +0900

    Feat-게임 규칙 추가

--- client/src/components/GameInfo.jsx
+++ client/src/components/GameInfo.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import CustomButton from "./CustomButton";
+import { useGlobalContext } from "../context";
+import { alertIcon, gameRules } from "../assets";
+import styles from "../styles";
 
 const GameInfo = () => {
-  return <div>GameInfo</div>;
+  const { contract, gameData, setShowAlert } = useGlobalContext();
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const navigate = useNavigate();
+
+  const handleBattleExit = async () => {};
+
+  return (
+    <>
+      <div className={styles.gameInfoIconBox}>
+        <div
+          className={`${styles.gameInfoIcon} ${styles.flexCenter}`}
+          onClick={() => setToggleSidebar(true)}
+        >
+          <img src={alertIcon} alt="info" className={styles.gameInfoIconImg} />
+        </div>
+      </div>
+
+      <div
+        className={`${styles.gameInfoSidebar} ${
+          toggleSidebar ? "translate-x-0" : "translate-x-full"
+        } ${styles.glassEffect} ${styles.flexBetween} backdrop-blur-3xl`}
+      >
+        <div className="flex flex-col">
+          <div className={styles.gameInfoSidebarCloseBox}>
+            <div
+              className={`${styles.flexCenter} ${styles.gameInfoSidebarClose}`}
+              onClick={() => setToggleSidebar(false)}
+            >
+              X
+            </div>
+          </div>
+          <h3 className={styles.gameInfoHeading}>게임 규칙:</h3>
+          <div className="mt-3">
+            {gameRules.map((rule, index) => (
+              <p key={`game-rule-index`} className={styles.gameInfoText}>
+                <span className="font-bold">{index + 1}</span>.{rule}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default GameInfo;
