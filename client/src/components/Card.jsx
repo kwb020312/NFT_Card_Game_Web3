6812cd13d767433faae1abefa456811bdb0435ba
N S <kwb020312@naver.com>
2023-05-14 13:55:37 +0900

    Feat-랜덤카드 추가

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
 
-const Card = () => {
-  return <div>Card</div>;
+import styles from "../styles";
+import { allCards } from "../assets";
+
+const generateRandomCardImage = () =>
+  allCards[Math.floor(Math.random() * (allCards.length - 1))];
+
+const img1 = generateRandomCardImage();
+const img2 = generateRandomCardImage();
+
+const Card = ({ card, title, restStyles, cardRef, playerTwo }) => {
+  return (
+    <div className={`${styles.cardContainer} ${restStyles}`}>
+      <img
+        src={playerTwo ? img2 : img1}
+        alt="card"
+        className={styles.cardImg}
+      />
+    </div>
+  );
 };
 
 export default Card;
