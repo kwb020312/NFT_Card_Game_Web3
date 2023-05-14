3e8b2ce024dc6558057bcf3631b3c53ae2faf64a
N S <kwb020312@naver.com>
2023-05-14 15:12:11 +0900

    Feat-전투중인 경우, Redirect

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -11,6 +11,7 @@ import { useNavigate } from "react-router-dom";
 
 import { ABI, ADDRESS } from "../contract";
 import { createEventListeners } from "./createEventListener";
+import { GetParams } from "../utils/onboard";
 
 const GlobalContext = createContext();
 
@@ -31,6 +32,7 @@ export const GlobalContextProvider = ({ children }) => {
   });
   const [updateGameData, setUpdateGameData] = useState(0);
   const [battleGround, setBattleGround] = useState("bg-astral");
+  const [step, setStep] = useState(1);
 
   const navigate = useNavigate();
 
@@ -41,6 +43,19 @@ export const GlobalContextProvider = ({ children }) => {
     } else {
       localStorage.setItem("battleground", battleGround);
     }
+  }, []);
+
+  useEffect(() => {
+    const resetParams = async () => {
+      const currentStep = await GetParams();
+
+      setStep(currentStep.step);
+    };
+
+    resetParams();
+
+    window?.ethereum?.on("chainChanged", () => resetParams());
+    window?.ethereum?.on("accountsChanged", () => resetParams());
   }, []);
 
   const updateCurrentWalletAddress = async () => {
@@ -73,7 +88,7 @@ export const GlobalContextProvider = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    if (contract) {
+    if (step !== -1 && contract) {
       createEventListeners({
         navigate,
         contract,
@@ -83,7 +98,7 @@ export const GlobalContextProvider = ({ children }) => {
         setUpdateGameData,
       });
     }
-  }, [contract]);
+  }, [contract, step]);
 
   useEffect(() => {
     if (showAlert?.status) {
